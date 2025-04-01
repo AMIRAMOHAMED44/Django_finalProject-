3f9054282aac2b9f36bfa759c2d2903ae153eed9
AMIRAMOHAMED44 <103863618+AMIRAMOHAMED44@users.noreply.github.com>
2025-04-01 14:45:13 +0200

    add database

--- final_project/settings.py
+++ final_project/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'final_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Django_finalProject',
+        'USER':'postgres',
+        'PASSWORD':'555',
+        'HOST':'localhost',
+        'PORT':5433
+
     }
 }
 
